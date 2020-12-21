16a1841362b0f751ec1ee23eec8d6637f4427669
Necdet Alperen Özil <48528832+alperenozil@users.noreply.github.com>
2020-12-21 18:43:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return "selam";
+  return (
+    <div>
+      <button className="btn">-</button>
+      <span>0</span>
+      <button>+</button>
+    </div>
+    
+  );
 }
 
 export default App;
